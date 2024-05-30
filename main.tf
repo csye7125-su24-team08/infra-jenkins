88af6d21314de8f92929e479e88803bd1f4dd0c8
Anuraag Bathula <bathula.anuraag.100@gmail.com>
2024-05-30 16:21:26 -0400

    Feature/a01 2 (#2)
    * Added dynamic dns record binding and parametrized code
    
    * Added ghactions workflow

--- main.tf
+++ main.tf
@@ -8,14 +8,12 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
-  shared_config_files = ["$HOME/.aws/config"]
-  shared_credentials_files = ["$HOME/.aws/credentials"]
-  profile = "infra"
+  region = var.region
+  profile = var.profile
 }
 
 resource "aws_vpc" "infra_vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.vpc_cidr
 
   tags = {
     Name = "infra_vpc"
@@ -28,47 +26,58 @@ resource "aws_internet_gateway" "infra_gw" {
   tags = {
     Name = "infra_gw"
   }
+
+  depends_on = [ aws_vpc.infra_vpc ]
 }
 
 resource "aws_subnet" "infra_subnet" {
   vpc_id            = aws_vpc.infra_vpc.id
-  cidr_block        = "10.0.0.0/24"
-  availability_zone = "us-east-1a"
+  cidr_block        = var.subnet_cidr
+  availability_zone = var.availability_zone
   
 
-  # tags = {
-  #   Name = "tf-example"
-  # }
+  tags = {
+    Name = "infra_subnet"
+  }
+
+  depends_on = [ aws_vpc.infra_vpc ]
 }
 
 resource "aws_route_table" "infra_rt" {
   vpc_id = aws_vpc.infra_vpc.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block = var.default_cidr
     gateway_id = aws_internet_gateway.infra_gw.id
   }
 
   route {
-    cidr_block = "10.0.0.0/16"
+    cidr_block = aws_vpc.infra_vpc.cidr_block
     gateway_id = "local"
   }
 
   tags = {
     Name = "tf_route_table"
   }
+
+  depends_on = [ aws_vpc.infra_vpc, aws_internet_gateway.infra_gw ]
 }
 
 resource "aws_route_table_association" "infra_rta" {
   subnet_id      = aws_subnet.infra_subnet.id
   route_table_id = aws_route_table.infra_rt.id
+
+  depends_on = [ aws_subnet.infra_subnet, aws_route_table.infra_rt ]
 }
 
 resource "aws_main_route_table_association" "infra_vpc_rta" {
   vpc_id         = aws_vpc.infra_vpc.id
   route_table_id = aws_route_table.infra_rt.id
+
+  depends_on = [ aws_vpc.infra_vpc, aws_route_table.infra_rt ]
 }
 
+# Will require in the future
 # resource "aws_network_interface" "infra_ni" {
 #   subnet_id   = aws_subnet.infra_subnet.id
 #   private_ips = ["10.0.0.10"]
@@ -78,7 +87,7 @@ resource "aws_main_route_table_association" "infra_vpc_rta" {
 #   }
 
 #   # tags = {
-#   #   Name = "primary_network_interface"
+#   #   Name = "tf_primary_network_interface"
 #   # }
 # }
 
@@ -105,6 +114,8 @@ EOF
 resource "aws_iam_instance_profile" "tf_jenkins_profile" {
   name = "tf_jenkins_profile"
   role = aws_iam_role.tf_jenkins_role.name
+
+  depends_on = [ aws_iam_role.tf_jenkins_role ]
 }
 
 resource "aws_iam_role_policy" "tf_jenkins_policy" {
@@ -129,18 +140,29 @@ resource "aws_iam_role_policy" "tf_jenkins_policy" {
     ]
 }
 EOF
+
+depends_on = [ aws_iam_role.tf_jenkins_role ]
 }
 
 resource "aws_default_security_group" "infra_dsg" {
   vpc_id = aws_vpc.infra_vpc.id
 
   ingress {
-    protocol  = "tcp"
+    protocol  = var.protocol
     self      = true
-    from_port = 80
-    to_port   = 443
+    from_port = var.https_port
+    to_port   = var.https_port
     # cidr_blocks = [aws_subnet.infra_subnet.cidr_block]
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.default_cidr]
+  }
+
+  ingress {
+    protocol  = var.protocol
+    self      = true
+    from_port = var.http_port
+    to_port   = var.http_port
+    # cidr_blocks = [aws_subnet.infra_subnet.cidr_block]
+    cidr_blocks = [var.default_cidr]
   }
 
   ingress {
@@ -150,67 +172,30 @@ resource "aws_default_security_group" "infra_dsg" {
     to_port   = 22
     # cidr_blocks = [aws_subnet.infra_subnet.cidr_block]
     cidr_blocks = ["0.0.0.0/0"]
-    # source = "18.206.107.24/29"
   }
 
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = var.jenkins_egress_from_port
+    to_port     = var.jenkins_egress_to_port
+    protocol    = var.jenkins_egress_protocol
+    cidr_blocks = [var.default_cidr]
   }
+
+  depends_on = [ aws_vpc.infra_vpc ]
 }
 
-# resource "aws_security_group" "infra_sg" {
-#   name        = "infra_sg"
-#   description = "Allow TLS inbound traffic and all outbound traffic"
-#   vpc_id      = aws_vpc.infra_vpc.id
-
-#   tags = {
-#     Name = "infra_sg"
-#   }
-# }
-
-# resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-#   security_group_id = aws_security_group.infra_sg.id
-#   cidr_ipv4         = aws_vpc.infra_vpc.cidr_block
-#   from_port         = 443
-#   ip_protocol       = "tcp"
-#   to_port           = 443
-# }
-
-# resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv6" {
-#   security_group_id = aws_security_group.infra_sg.id
-#   cidr_ipv6         = aws_vpc.infra_vpc.ipv6_cidr_block
-#   from_port         = 443
-#   ip_protocol       = "tcp"
-#   to_port           = 443
-# }
-
-# resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-#   security_group_id = aws_security_group.infra_sg.id
-#   cidr_ipv4         = "0.0.0.0/0"
-#   ip_protocol       = "-1" # semantically equivalent to all ports
-# }
-
-# resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
-#   security_group_id = aws_security_group.infra_sg.id
-#   cidr_ipv6         = "::/0"
-#   ip_protocol       = "-1" # semantically equivalent to all ports
-# }
-
 resource "aws_instance" "tf_jenkins" {
-  ami               = "ami-0824b7052bb75fb6d"
-  availability_zone = "us-east-1a"
-  instance_type     = "t2.micro"
+  ami               = var.jenkins_ami
+  availability_zone = var.availability_zone
+  instance_type     = var.instance_type
   subnet_id     = aws_subnet.infra_subnet.id
   iam_instance_profile = aws_iam_instance_profile.tf_jenkins_profile.name
 
   user_data = <<-EOL
 #!/bin/bash -xe
 cd /home/ubuntu
-touch file.txt
+touch abc.txt
 caddy stop
 caddy fmt --overwrite
 sudo ./caddy run
@@ -219,10 +204,29 @@ EOL
   tags = {
     Name = "tf-jenkins"
   }
+
+  depends_on = [ aws_subnet.infra_subnet, aws_iam_instance_profile.tf_jenkins_profile ]
 }
 
+data "aws_eip" "infra_eip" {
+  id = var.infra_eip
+}
+
+data "aws_route53_zone" "infra_zone" {
+  zone_id = var.infra_zone
+}
 
 resource "aws_eip_association" "eip_assoc" {
   instance_id   = aws_instance.tf_jenkins.id
-  allocation_id = "eipalloc-00d408691ab4dcba1"
+  allocation_id = data.aws_eip.infra_eip.id
+
+  depends_on = [ aws_instance.tf_jenkins ]
+}
+
+resource "aws_route53_record" "jenkins_dns_rec" {
+  zone_id = data.aws_route53_zone.infra_zone.zone_id
+  name    = data.aws_route53_zone.infra_zone.name
+  type    = "A"
+  ttl     = var.jenkins_dns_ttl
+  records = [data.aws_eip.infra_eip.public_ip]
 }
